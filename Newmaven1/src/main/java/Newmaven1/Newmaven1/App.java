6dfb88c88c5589bb0553a94a91042fdfed99691e
SourabhKaujalgi <50537576+SourabhKaujalgi@users.noreply.github.com>
2020-02-07 19:36:07 +0530

    Update App.java

--- Newmaven1/src/main/java/Newmaven1/Newmaven1/App.java
+++ Newmaven1/src/main/java/Newmaven1/Newmaven1/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worldnew" );
+        System.out.println( "Hello Worldnew 4 min" );
         
     }
 }
